f29609b8018866d64ce42ab14f0017aa2d3a95db
Shefaa Saad <somar0009@stu.kau.edu.sa>
2019-01-15 17:18:16 +0300

    Initial commit

---
+++ FirstFile.py
@@ -0,0 +1,19 @@
+print("I'm OK Shefaa!")
+
+# Variables Lesson
+
+# + - / * % ** //
+# The last one is called Flow Division which is resulted with an integer division outcome
+
+number1 = 10
+number2 = 7
+
+number3 = number1 + number2
+
+print("The result is: ",number3," ?")
+
+myName = "Shefaa"
+shefaaInCapital = myName.capitalize()
+shefaaCentered = (' '+myName+' ').center(20,'.')
+saadCentered = " Saad ".center(20,'.')
+print(shefaaCentered+"\n"+saadCentered)
